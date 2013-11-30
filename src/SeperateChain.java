426d19fd7668ffeed9862474623fa6ab03187ef0
sandrodago <dagostino.alex@gmail.com>
2013-11-30 18:39:01 -0500

    removed copy of collision function

--- src/SeperateChain.java
+++ src/SeperateChain.java
@@ -9,18 +9,13 @@ public class SeperateChain
 		// in here we will do the steps for the algorithm
 	}
 
-	private void collision(int key)
-	{
-		//in here we will deal with collisions
-	}
-
 	private void makeNewList(List l)
 	{
 		//in here we will make a new list for the colided key
 	}
 
 
-	private int collision()
+	private int collision(String key)
 	{
 		/*
 			while (currentNode.next != null) {
