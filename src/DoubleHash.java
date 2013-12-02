041b04d8bbcb57527cfe391639d78ef1ddb2fd42
sandrodago <dagostino.alex@gmail.com>
2013-12-01 22:58:16 -0500

    fixed all compilation errors. adjusted load factor and expansion number.

--- src/DoubleHash.java
+++ src/DoubleHash.java
@@ -1,57 +1,144 @@
+import java.lang.Math;
+
 public class DoubleHash{
 
-	public void DoubleHash (){};
+	public int collisionCounter=0;
+	public char emptyMarkerScheme;
+	public String[] table;
+    private int rehashFactor=1;
+    public int elementsInTable=0;
+    public boolean expandByFactor=true;    
+    public final int EXPAND_FACTOR_VARIABLE=2;
+    public final int EXPAND_BY_NUMBER_VARIABLE=100;
+    public double loadFactor=.51;
 
-	private char emptyMarkerScheme=null;
+	public DoubleHash ()
+	{
+		this.table = new String[100];
+	};
 
-	public void doubleHash(String s) {
-		Hash h = new Hash();
-        int key = h.hash(s);
+	public void doubleHash(String s) 
+	{
+        int key = this.hash(s);
         int hv1 = hv1(key);
         int hv2 = hv2(key);
 
-        if ( h.table[hv1] == null)
+        if ( this.table[hv1] == null)
         {
-            h.put (s, hv1);
+            this.put (s, hv1);
         }
         else
-        {   
-           h.put (s, collision(key, hv2));
+        {
+			// System.out.println("Collided");
+           	this.put (s, collision(key, hv2));
         }
     }
 
-    
-    private int hv1(int i){
-        return i % table.length;
+
+    private int hv1(int i)
+    {
+    	// System.out.println("----- i! " + i + " -----");
+    	// System.out.println("----- table length ! " + this.table.length + " -----");
+    	// System.out.println("----- i mod ! " + i % this.table.length + " -----");
+    	return i % this.table.length;
     }
 
-    
+
     private int hv2(int i){
         DoubleHashPrime prime = new DoubleHashPrime();
-        int primeNum1 = prime.findPerfectPrime(table.length);
+        int primeNum1 = prime.findPerfectPrime(this.table.length);
         int primeNum2 = prime.findPerfectPrime(primeNum1);
         int num = (primeNum2*i)%primeNum1;
         return num;
     }
 
 
-    private int collision(int hv1, int hv2){
+    private int collision(int hv1, int hv2)
+    {
         int i = 0;
         int newIndex = 0;
         Hash h = new Hash();
         do{
             collisionCounter++;
             ++i;
-            newIndex = (hv1+(i*hv2)) % h.table.length;
-        }while(h.table[newIndex] != null && i < h.table.length);
+            newIndex = (hv1+(i*hv2)) % this.table.length;
+        }while(this.table[newIndex] != null && i < this.table.length);
         return newIndex;
     }
 
-    private String setEmptyMarkerScheme(String type)
+    public void setEmptyMarkerScheme(char type)
     {
     	this.emptyMarkerScheme = type;
     }
 
+
+    private void put(String value, int key)
+    {
+    	// System.out.println("----- table * loadfactor ! " + this.table.length*loadFactor + " -----");
+    	// System.out.println("----- elements in table  ! " + elementsInTable + " -----");
+
+
+        if (elementsInTable+1 > this.table.length*loadFactor)
+        {
+            this.rehash();
+        }
+     //    System.out.println("----- I'm inserting here: "+key+" -----");
+    	// System.out.println("----- table lenth ! " + this.table.length + " -----");
+        this.table[key] = value;
+        // System.out.println("----- Array index Post Insert ! " + this.table[key] + " -----");
+        ++elementsInTable;
+    }
+
+
+    private void rehash ()
+    {
+    	int newSize;
+    	if (expandByFactor)
+    	{
+    	// System.out.println("----- table lenth ! " + this.table.length + " -----");
+
+    	    newSize = this.table.length*EXPAND_FACTOR_VARIABLE;
+
+    	// System.out.println("----- NEW table lenth ! " + newSize + " -----");
+
+    	}
+    	else
+    	{
+    	    newSize = this.table.length+EXPAND_BY_NUMBER_VARIABLE;
+    	}
+        String old[] = this.table;
+        this.table = new String[newSize];
+        for (int i = 0; i < old.length; ++i)
+        {
+        	if(old[i] != null)
+        	{
+        		this.doubleHash(old[i]);
+        	}
+            // TODO Call double hash insrt with old[i] into table[]
+        }
+        old = null;
+    }
+
+
+    private void setRehashFactor(int factorOrNumber)
+    {
+        rehashFactor = factorOrNumber;
+    }
+
+    private int hash(String str)
+    {
+        int nHash=7;
+        // System.out.println("----- Hashing string ! " + str + " -----");
+        // System.out.println("----- with length: " + str.length() + " -----");
+        
+        for (int i=0; i < str.length(); i++)
+        {
+            nHash = Math.abs(nHash*31+str.charAt(i));
+        }
+        // System.out.println("----- Converted to: " + nHash + " -----");
+        
+        return nHash;
+    }
 
 
 
--- src/SeperateChain.java
+++ src/SeperateChain.java
@@ -1,29 +1,99 @@
-public class SeperateChain 
+import java.lang.Math;
+public class SeperateChain
 {
-	
-	public void SeperateChain(){}
 
-	
-	public String seperateChain(String s)
+	public Node[] table;
+    private int rehashFactor=1;
+    public int elementsInTable=0;
+    public boolean expandByFactor=true;
+    public final int EXPAND_FACTOR_VARIABLE=2;
+    public final int EXPAND_BY_NUMBER_VARIABLE=100;
+    public double loadFactor=.51;
+    public int collisionCounter=0;
+
+	public SeperateChain()
 	{
-		return s;
-		// in here we will do the steps for the algorithm
+		this.table = new Node[100];
 	}
 
-	private void makeNewList(String[] l)
+
+	public void seperateChain(String s)
 	{
-		//in here we will make a new list for the colided key
+		int key = this.hash(s);
+		//System.out.println("Got " + key + " from hash.");
+		// System.out.println("Got " + table.length + " as a table length.");
+		key = key % this.table.length;
+		//System.out.println("Got " + key + " from modding.");
+        Node newNode = new Node(s,key);
+		put (newNode, key);
 	}
 
+	private void put(Node value, int key)
+    {
 
-	private int collision(String key)
-	{
-		/*
-			while (currentNode.next != null) {
+        if (elementsInTable+1 > table.length*loadFactor)
+        {
+            rehash();
+        }
+
+     	if (this.table[key] == null)
+        {
+            this.table[key] = value;
+        }
+        else
+        {
+        	++collisionCounter;
+            Node currentNode = this.table[key];
+            while(currentNode.next != null)
+            {
                 currentNode = currentNode.next;
             }
-            currentNode.next = s;
-        */
-	}
+            currentNode.next = value;
+        }
+        ++elementsInTable;
+    }
+
+
+    private void rehash ()
+    {
+    	int newSize;
+    	if (expandByFactor)
+    	{
+    	    newSize = this.table.length*EXPAND_FACTOR_VARIABLE;
+    	}
+    	else
+    	{
+    	    newSize = this.table.length+EXPAND_BY_NUMBER_VARIABLE;
+    	}
+    	collisionCounter = 0;
+        Node old[] = this.table;
+        this.table = new Node[newSize];
+        for (int i = 0; i < old.length; ++i)
+        {
+        	if (old[i] != null)
+        	{
+        		this.seperateChain(old[i].value);
+        	}
+            // TODO Call double hash insrt with old[i] into table[]
+        }
+        old = null;
+    }
+
+
+    private int hash(String str)
+    {
+        int nHash=7;
+        //System.out.println("----- Hashing string ! " + str + " -----");
+        //System.out.println("----- with length: " + str.length() + " -----");
+        
+        for (int i=0; i < str.length(); i++)
+        {
+            nHash = Math.abs(nHash*31+str.charAt(i));
+        }
+        //System.out.println("----- Converted to: " + nHash + " -----");
+        
+        return nHash;
+    }
+
 
 }
